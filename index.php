a26863ca45c357e22a5313a7bb27d06985dba115
Krugli <75218175+Krugli@users.noreply.github.com>
2021-06-29 18:06:09 +0300

    Главный файл

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <p> Список лаб </p>
+
+    <ul id="nav">
+        <li><a href="lab1">Лаба 1</a>
+        <li><a href="lab2">Лаба 2</a>
+        <li><a href="lab3">Лаба 3</a>
+        <li><a href="lab4">Лаба 4</a>
+        <li><a href="lab5">Лаба 5</a>
+    </ul>
+
+</html>
