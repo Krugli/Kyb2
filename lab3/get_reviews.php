ba63f894c6d2114130fd3d7e0ca636a77b7a4e56
Krugli <75218175+Krugli@users.noreply.github.com>
2021-10-11 14:29:06 +0300

    Обновил задание 2 лабы 3

--- lab3/get_reviews.php
+++ lab3/get_reviews.php
@@ -21,9 +21,12 @@
     }
 
     foreach($needed_section->getElementsByTagName("comment") as $item){
-        echo "<p><section style='background-color:rgb(192, 227, 255); font-size:22px'>";
-        echo $item->attributes[0]->nodeValue ." (". $item->attributes[1]->nodeValue . ")";
-        echo " >> ". $item->nodeValue;
-        echo "</section></p>";
+        echo "<div style='background-color:rgb(192, 227, 255);padding-bottom:10px; padding-top:10px'>";
+            echo "<section style='background-color:rgb(19, 207, 235); margin-left:40%; width:20%; padding-bottom:10px; padding-top:10px' align='center'>";
+            echo "<div style='font-size:22px'>".$item->attributes[0]->nodeValue."</div>";
+            echo "<div style='font-size:12px'>".$item->attributes[1]->nodeValue."</div>";
+            echo "<div style='font-size:22px'>".$item->nodeValue."</div>";
+            echo "<section style='background-color:rgb(192, 227, 255); padding-bottom=10px; padding-top=10px' align='center'>";
+        echo "</div>";
     }
 ?>
